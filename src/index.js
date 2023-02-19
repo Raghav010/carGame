3ac2a58ab4d3f1b0621b02eab7c305a8a16f04fa
raghav <raghavdonakanti@gmail.com>
2023-02-20 01:54:21 +0530

    added start screen,game over screen and stats HUD; supports trueFuelLoss

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const minimap_width=window.innerWidth/6;
 const minimap_height=window.innerWidth/6;
 const third_CamHeight=0.45;
 const driver_CamHeight=0.15;
-const turning_radius=2;
+const turning_radius=1.3;
 const third_CamTarget=6;
 const driver_CamTarget=13;
 // horizontal cam in code below
@@ -25,57 +25,67 @@ let gasCansOnTrack=0; // length of arr?
 // [[gasCanObject,itsBoundingBox]]
 const gasCanArr=[];
 let health=100;
+const trueFuelLoss=1;
+const accFuelDrop=0.09;
+const fuelCap=110;
 let fuel=110; // litres
 let gasCanFuel=5; // litres
 // score?? time??
 let fuelDist=0.81; // how far you can go on 1 litre
+let loader=null;
+let cons_stadiumData=null;
+let gasCan=null;
 
 
 
 
 
-
-// initial camera and scene setup---------------------------------------------
-// creating the scene and camera
-const scene=new THREE.Scene();
-const camera=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-
-// creating minimap cam
-const miniCam=new THREE.OrthographicCamera(-3,3,3,-3,0.1,1000);
-
-// normal renderer
-const renderer=new THREE.WebGLRenderer({antialias:true});
-renderer.setSize(window.innerWidth,window.innerHeight);
-renderer.setClearColor("#87CEEB");
-document.body.appendChild(renderer.domElement);
-
-
-//mini-map renderer
-const miniRenderer=new THREE.WebGLRenderer({antialias:true});
-miniRenderer.setSize(minimap_width,minimap_height);
-miniRenderer.domElement.style.position="absolute";
-miniRenderer.domElement.style.top=20+"px";
-miniRenderer.domElement.style.left=20+"px";
-document.body.appendChild(miniRenderer.domElement);
-
-
-function setCameraPos(x,y,z,lookAtVec,cam)
+// loading all the meshes first-----------------------------------------------
+async function loadMeshes()
 {
-    if(cam=="normal")
-    {
-        camera.position.x=x;
-        camera.position.y=y;
-        camera.position.z=z;
-        camera.lookAt(lookAtVec);
-    }
-    else
-    {
-        miniCam.position.x=x;
-        miniCam.position.y=y;
-        miniCam.position.z=z;
-        miniCam.lookAt(lookAtVec);
-    }
+    // loading all meshes into the app
+    loader=new GLTFLoader();
+    cons_stadiumData=await loader.loadAsync("../Meshes/full_stadium_myTrack.glb");
+    gasCan=await loader.loadAsync("../Meshes/gas-can.glb");
 }
+loadMeshes(); // calling before start page loads, so meshes can load in the background
+
+
+
+
+
+
+
+// starting page-------------------------------------------------------
+let startButton=document.createElement("button");
+startButton.innerText="Start."
+startButton.style.width="200px";
+startButton.style.height="100px";
+startButton.style.borderRadius="20%";
+startButton.style.fontSize="30px";
+startButton.style.fontWeight="bold";
+startButton.style.position="absolute";
+startButton.style.top="200px";
+startButton.style.left="45%";
+startButton.style.backgroundColor="#51AAAD";
+startButton.onclick=startGame;
+
+let start=document.createElement("div");
+start.style.width="100vw";
+start.style.height="100vh";
+start.style.backgroundImage="url('../images/start_screen.png')";
+start.appendChild(startButton);
+document.body.appendChild(start);
+
+
+function startGame()
+{
+    document.body.removeChild(start);
+    main();
+}
+
+
+
 
 
 
@@ -86,6 +96,90 @@ function setCameraPos(x,y,z,lookAtVec,cam)
 // main function ----------------------------------------
 async function main()
 {
+
+    let animationFrameid=null;
+
+
+
+
+    // initial camera and scene setup---------------------------------------------
+    // creating the scene and camera
+    const scene=new THREE.Scene();
+    const camera=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+
+    // creating minimap cam
+    const miniCam=new THREE.OrthographicCamera(-5,5,5,-5,0.1,1000);
+
+    // normal renderer
+    const renderer=new THREE.WebGLRenderer({antialias:true});
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    renderer.setClearColor("#51AAAD");
+    document.body.appendChild(renderer.domElement);
+
+
+    //mini-map renderer
+    const miniRenderer=new THREE.WebGLRenderer({antialias:true});
+    miniRenderer.setSize(minimap_width,minimap_height);
+    miniRenderer.domElement.style.position="absolute";
+    miniRenderer.domElement.style.top=20+"px";
+    miniRenderer.domElement.style.left=20+"px";
+    document.body.appendChild(miniRenderer.domElement);
+
+
+    function setCameraPos(x,y,z,lookAtVec,cam)
+    {
+        if(cam=="normal")
+        {
+            camera.position.x=x;
+            camera.position.y=y;
+            camera.position.z=z;
+            camera.lookAt(lookAtVec);
+        }
+        else
+        {
+            miniCam.position.x=x;
+            miniCam.position.y=y;
+            miniCam.position.z=z;
+            miniCam.lookAt(lookAtVec);
+        }
+    }
+
+    // setting up stats
+    let healthHUD=document.createElement("p"); 
+    healthHUD.innerText="Health: "+health;
+    healthHUD.style.fontSize="35px"
+    healthHUD.style.fontWeight="bold";
+    healthHUD.style.color="white";
+    healthHUD.style.margin="0px";
+    let fuelHUD=document.createElement("p");
+    fuelHUD.innerText="Fuel: "+fuel;
+    fuelHUD.style.fontSize="35px"
+    fuelHUD.style.fontWeight="bold";
+    fuelHUD.style.color="white";
+    fuelHUD.style.margin="0px";
+    let timeHUD=document.createElement("p");
+    timeHUD.innerText="Time: 0";
+    timeHUD.style.fontSize="35px"
+    timeHUD.style.fontWeight="bold";
+    timeHUD.style.color="white";
+    timeHUD.style.margin="0px";
+    let statBox=document.createElement("div");
+    statBox.style.position="absolute";
+    statBox.style.top="20px";
+    statBox.style.right="20px";
+    statBox.appendChild(healthHUD);
+    statBox.appendChild(fuelHUD);
+    statBox.appendChild(timeHUD);
+    
+    
+
+
+
+
+
+
+
+
 
     // More detailed configuration ----------------------------------------------------
     // adding lighting
@@ -99,15 +193,12 @@ async function main()
 
     
 
-    // loading all meshes into the app
-    const loader=new GLTFLoader();
-    const cons_stadiumData=await loader.loadAsync("../Meshes/full_stadium_myTrack.glb");
+   
     //console.log(cons_stadiumData)
     const car=cons_stadiumData.scene.children[6];
     const track=cons_stadiumData.scene.children[2];
     const outer_stadium=cons_stadiumData.scene.children[1];
     const inner_stadium=cons_stadiumData.scene.children[0];
-    let gasCan=await loader.loadAsync("../Meshes/gas-can.glb");
     gasCan=gasCan.scene;
     
 
@@ -210,6 +301,10 @@ async function main()
 
 
 
+
+
+
+
     // all per frame update functions---------------------------------------------------
     // moves car and the bounding box
     function moveCar()
@@ -226,10 +321,14 @@ async function main()
         }
         if (keysPressed["w"]) {
             carVel+=((carPower)*delta);
+            if(trueFuelLoss)
+                fuel-=accFuelDrop;
             //car.position+=(carLookDir*0.05);
         }
         if (keysPressed["s"]) {
             carVel+=((-carBrakePower)*delta);
+            if(trueFuelLoss)
+                fuel-=accFuelDrop;
             //car.position.z-=(carLookDir*0.05);
         }
         if(carVel!=0 && !(carVel<0.001 && carVel>-0.001))
@@ -239,9 +338,13 @@ async function main()
             car.position.add(carLookDir.clone().multiplyScalar(carVel*delta));
 
             // reducing fuel(maybe not update it every frame???)
-            fuel-=((carVel*delta)/fuelDist);
-            if(fuel < 0 )
-                console.log("You ran out of fuel");
+            if(!trueFuelLoss)
+            {
+                fuel-=((carVel*delta)/fuelDist);
+                if(fuel < 0 )
+                    console.log("You ran out of fuel");
+            }
+
 
             // checking for out of bounds(out of stadium)
             if(car.position.x>=16.3 || car.position.x <=-24.5)
@@ -312,36 +415,85 @@ async function main()
                 gasCanArr.splice(i,1);
                 i--;
                 gasCansOnTrack--;
-                fuel+=gasCanFuel;
+                if(fuel<fuelCap)
+                    fuel+=gasCanFuel;
                 console.log("Fuel",fuel);
             }
         }
         spawnGasCans();
     }
 
+    // updates stats and checks for game over
+    function updateStats()
+    {
+        if(fuel<=0)
+        {
+            cancelAnimationFrame(animationFrameid);
+            while (document.body.firstChild) {
+                document.body.removeChild(document.body.firstChild);
+            }
+            gameOver(1,0,0);
+        }
+        fuelHUD.innerText="Fuel: "+Math.trunc(fuel);
+        healthHUD.innerText="Health: "+health;
+        timeHUD.innerText="Time: "+(Math.floor(Date.now() / 1000)-startTime);
+    }
 
 
 
 
 
 
-
-    // the animation loop---------------------------------------------
+    // the animation loop------------------------------------------------------
     // setting up the clock
     let clock=new THREE.Clock();
-
+    let startTime=Math.floor(Date.now() / 1000);
+    // adding stats to screen
+    document.body.appendChild(statBox);
 
     // animation loop
     function animate(){
-        requestAnimationFrame(animate);
+        animationFrameid=requestAnimationFrame(animate);
         moveCar();
         moveCamera(viewMode,"normal");
         moveCamera("minimap","minimap");
         fuelUp();
         renderer.render(scene,camera);
         miniRenderer.render(scene,miniCam);
+        updateStats();
     }
     animate();
+
+
+
+
+
+
+
+    // game over screen--------------------------------------------
+    function gameOver(fuelOver,healthOver,won)
+    {
+        console.log("entered game over screen");
+        let message=document.createElement("p");
+        if(fuelOver)
+            message.innerText="You ran out of fuel :(";
+        else if(healthOver)
+            message.innerText="Your car blew up";
+        else if(won)
+            message.innerText="You Finished.";
+        message.style.position="absolute";
+        message.style.left="300px";
+        message.style.top="45%";
+        message.style.color="white";
+        message.style.fontSize="60px";
+        message.style.fontWeight="bold";
+        let go=document.createElement("div");
+        go.style.width="100vw";
+        go.style.height="100vh";
+        go.style.backgroundImage="url('../images/game_overScreen.png')";
+        go.appendChild(message);
+        document.body.appendChild(go);
+    }
     
 }
 
@@ -349,5 +501,4 @@ async function main()
 
 
 
-main();
 
